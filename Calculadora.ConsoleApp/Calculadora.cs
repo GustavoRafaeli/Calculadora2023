62d1306b4c28afa7003da274f656038214bcf28b
Gustavo Rafaeli <gustavo.rafaeli.oliv@gmail.com>
2023-03-13 14:33:58 -0300

    Add project files.

---
+++ Calculadora.ConsoleApp/Calculadora.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.ConsoleApp.Entities
+{
+    public class Calculo
+    {
+        public double Valor1 { get; set; }
+
+        public double Valor2 { get; set; }
+
+        public double Resultado { get; set; }
+
+        public Calculo(double valor1, double valor2)
+        {
+            Valor1 = valor1;
+            Valor2 = valor2;
+        }
+
+        public void Soma()
+        {
+            Resultado = Valor1 + Valor2;
+        }
+
+        public void Sub()
+        {
+            Resultado = Valor1 - Valor2;
+        }
+
+        public void Div()
+        {
+            Resultado = Valor1 / Valor2;
+        }
+
+        public void Mult()
+        {
+            Resultado = Valor1 * Valor2;
+        }
+    }
+}
---
+++ Calculadora.ConsoleApp/Program.cs
@@ -0,0 +1,91 @@
+﻿using Calculadora.ConsoleApp.Entities;
+
+namespace Calculadora.ConsoleApp
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("(1) para somar;");
+                Console.WriteLine("(2) para subtrair;");
+                Console.WriteLine("(3) para multiplicar;");
+                Console.WriteLine("(4) para dividir;");
+                Console.WriteLine("(S) para sair;");
+
+                String option = Console.ReadLine();
+
+                if (option.ToUpper() == "S")
+                {
+                    break;
+                }
+
+                if (option != "1" && option != "2" && option != "3" && option != "4" && option != "S")
+                {
+                    Console.WriteLine("Operação Inválida, tente novamente.....");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                Console.WriteLine("Digite o primeiro valor:");
+                double valor1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o segundo valor:");
+                double valor2 = double.Parse(Console.ReadLine());
+
+                Calculo calculo = new Calculo(valor1, valor2);
+
+                switch (option.ToUpper())
+                {
+                    case "1":
+                        calculo.Soma();
+                        Console.WriteLine("O resultado é: " + calculo.Resultado.ToString("N2"));
+                        Console.WriteLine();
+                        break;
+
+                    case "2":
+                        calculo.Sub();
+                        Console.WriteLine("O resultado é: " + calculo.Resultado.ToString("N2"));
+                        Console.WriteLine();
+                        break;
+                    case "3":
+                        calculo.Mult();
+                        Console.WriteLine("O resultado é: " + calculo.Resultado.ToString("N2"));
+                        Console.WriteLine();
+                        break;
+
+                    case "4":
+                        {
+                            while (valor2 == 0)
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine("Não pode dividir por 0, Digite novamente.");
+                                Console.WriteLine();
+
+                                Console.WriteLine("Digite o primeiro valor:");
+                                valor1 = double.Parse(Console.ReadLine());
+                                Console.WriteLine("Digite o segundo valor:");
+                                valor2 = double.Parse(Console.ReadLine());
+
+                                calculo = new Calculo(valor1, valor2);
+
+                            }
+
+                            Console.WriteLine();
+                            calculo.Div();
+                            Console.WriteLine("O resultado é: " + calculo.Resultado.ToString("N2"));
+                            Console.WriteLine();
+                        }
+                        break;
+
+                        Console.ReadLine();
+                        Console.Clear();
+                }
+
+            }
+
+        }
+    }
+}
